d1d4984ccb3c9eaca4699a6a55bbf5bd08eb657b
spacelan <spacelan1993@gmail.com>
2019-05-11 02:08:27 +0800

    222

--- .travis/windows.sh
+++ .travis/windows.sh
@@ -16,7 +16,7 @@ cmake --build . --config Release
 
 rm -rf ./*
 
-cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax86 ../base
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax86 -DCMAKE_PREFIX_PATH=../vendors/libv8-7.2-windows-x86 ../base
 
 cmake --build . --config Debug
 
