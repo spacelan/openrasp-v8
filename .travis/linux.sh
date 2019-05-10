070b795ea0567b472c0ca0b71b1adc993ecbe8b8
spacelan <spacelan1993@gmail.com>
2019-05-11 02:41:48 +0800

    222

--- .travis/linux.sh
+++ .travis/linux.sh
@@ -14,7 +14,7 @@ popd
 
 mkdir buildx32 && pushd buildx32
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../vendors/libv8-7.2-linux-x86;../vendors/libc++-linux-x86" ../base
+cmake -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../vendors/libv8-7.2-linux-x86;../vendors/libc++-linux-x86" ../base
 
 make VERBOSE=1 -j
 
