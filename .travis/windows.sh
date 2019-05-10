6ef71ca05f062c97ccc9b242271634c0f7c27641
spacelan <spacelan1993@gmail.com>
2019-05-11 02:16:13 +0800

    222

--- .travis/windows.sh
+++ .travis/windows.sh
@@ -4,7 +4,7 @@ set -ex
 
 mkdir build && pushd build
 
-cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax64 ../base
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_PREFIX_PATH=../vendors/libv8-7.2-windows-x86 ../base
 
 cmake --build . --config Release
 
@@ -12,7 +12,7 @@ cmake --build . --config Release
 
 rm -rf ./*
 
-cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax86 -DCMAKE_PREFIX_PATH=../vendors/libv8-7.2-windows-x86 ../base
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax64 ../base
 
 cmake --build . --config Release
 
