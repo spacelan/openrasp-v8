386223fa5769376a5f7c3ab0caed45415fa187dc
spacelan <spacelan1993@gmail.com>
2019-05-11 02:03:48 +0800

    222

--- .travis/windows.sh
+++ .travis/windows.sh
@@ -6,6 +6,22 @@ mkdir build && pushd build
 
 cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax64 ../base
 
+cmake --build . --config Debug
+
 cmake --build . --config Release
+
+./Debug/tests "~Exception"
+
+./Release/tests "~Exception"
+
+rm -rf ./*
+
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax86 ../base
+
+cmake --build . --config Debug
+
+cmake --build . --config Release
+
+./Debug/tests "~Exception"
 
 ./Release/tests "~Exception"
