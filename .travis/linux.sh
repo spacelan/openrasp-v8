8e6b8697e17ab6c358dd41d3551d5e0e00111222
lanyuhang <lanyuhang@baidu.com>
2019-05-16 14:48:22 +0800

    travis build java

---
+++ .travis/linux.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+set -ex
+
+tar zxf vendors/centos6-sysroot.tar.gz -C /tmp/
+
+mkdir buildx64 && pushd buildx64
+
+(source /tmp/centos6-sysroot/setx64.sh && cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../java)
+
+make VERBOSE=1 -j
+
+popd
+
+mkdir buildx86 && pushd buildx86
+
+(source /tmp/centos6-sysroot/setx86.sh && cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-m32 $CXXFLAGS" -DCMAKE_PREFIX_PATH="../vendors/libc++-linux-x86;../vendors/libv8-7.2-linux-x86" ../java)
+
+make VERBOSE=1 -j
+
+popd
+
+mkdir dist
+
+cp buildx64/libopenrasp_v8_java.so dist/libopenrasp_v8_java.so
+
+cp buildx86/libopenrasp_v8_java.so dist/libopenrasp_v8_java.so.x86
---
+++ .travis/windows.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+choco install -y jdk8 -params "both=true"
+
+mkdir buildx64 && pushd buildx64
+
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax64 ../java
+
+cmake --build . --config Release
+
+popd
+
+mkdir buildx86 && pushd buildx86
+
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_PREFIX_PATH="../vendors/libv8-7.2-windows-x86"  ../java
+
+cmake --build . --config Release
+
+popd
+
+mkdir dist
+
+cp buildx64/Release/openrasp_v8_java.dll dist/openrasp_v8_java.dll
+
+cp buildx86/Release/openrasp_v8_java.dll dist/openrasp_v8_java.dll.x86
