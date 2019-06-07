2bdb6c1ee5d4f0c9289666d2ac69f3fe90467115
spacelan <spacelan1993@gmail.com>
2019-06-08 02:31:07 +0800

    test java

--- .travis/windows.sh
+++ .travis/windows.sh
@@ -4,6 +4,8 @@ set -ex
 
 choco install -y jdk8 -params "both=true"
 
+choco install -y maven
+
 mkdir build64 && pushd $_
 
 cmake -DCMAKE_VERBOSE_MAKEFILE=ON -Ax64 ../java
@@ -20,11 +22,17 @@ cmake --build . --config RelWithDebInfo
 
 popd
 
-mkdir dist && pushd $_
+pushd java/src/main/resources
 
-mkdir -p natives/windows_32 && cp ../build32/RelWithDebInfo/openrasp_v8_java.dll $_
-mkdir -p natives/windows_64 && cp ../build64/RelWithDebInfo/openrasp_v8_java.dll $_
+mkdir -p natives/windows_32 && cp ${TRAVIS_BUILD_DIR}/build32/RelWithDebInfo/openrasp_v8_java.dll $_
+mkdir -p natives/windows_64 && cp ${TRAVIS_BUILD_DIR}/build64/RelWithDebInfo/openrasp_v8_java.dll $_
 
 tar zcf java_natives_windows.tar.gz natives
 
-rm -rf natives
+popd
+
+pushd java
+
+JAVA_HOME="/c/Program Files (x86)/Java/jdk1.8.0_211" mvn test
+
+JAVA_HOME="/c/Program Files/Java/jdk1.8.0_211" mvn test
