359ebba8d11cb792302bca1330673fe4fce79846
lanyuhang <lanyuhang@baidu.com>
2019-07-18 11:25:53 +0800

    auth download

--- java/fetch_native_libraries.sh
+++ java/fetch_native_libraries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ev
+set -evx
 ROOT=$(git rev-parse --show-toplevel)
 TAG=$(git describe --tags --abbrev=0)
 if [ $TRAVIS ]; then
@@ -8,9 +8,9 @@ if [ $TRAVIS ]; then
 else
   DIR=/tmp
 fi
-wget --header "$GITHUB_AUTH_HEADER" -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
-wget --header "$GITHUB_AUTH_HEADER" -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
-wget --header "$GITHUB_AUTH_HEADER" -N -P $DIR https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
+curl -L -H "$GITHUB_AUTH_HEADER" -o $DIR/java_natives_linux.tar.gz -z $DIR/java_natives_linux.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_linux.tar.gz
+curl -L -H "$GITHUB_AUTH_HEADER" -o $DIR/java_natives_osx.tar.gz -z $DIR/java_natives_osx.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_osx.tar.gz
+curl -L -H "$GITHUB_AUTH_HEADER" -o $DIR/java_natives_windows.tar.gz -z $DIR/java_natives_windows.tar.gz https://github.com/baidu-security/openrasp-v8/releases/download/$TAG/java_natives_windows.tar.gz
 tar zxf $DIR/java_natives_linux.tar.gz -C $ROOT/java/src/main/resources
 tar zxf $DIR/java_natives_osx.tar.gz -C $ROOT/java/src/main/resources
 tar zxf $DIR/java_natives_windows.tar.gz -C $ROOT/java/src/main/resources
